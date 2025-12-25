ba9f902f1bccd70f6f3b4410f8b99a45b1a85c9c
EchoRavenX <MagaPenguin@proton.me>
2025-12-25 07:59:24 -0500

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,19 +4,16 @@ set -ouex pipefail
 
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-
+dnf5 remove firefox
+dbf5 remove firefox-langpacks
 dnf5 -y copr enable ublue-os/staging
 dnf5 swap ffmpeg-free ffmpeg --allowerasing
 
  dnf5 update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
  dnf5 install tlp tlp-rdw
 
-#### Example for enabling a System Unit File
+#### System Unit File
 
 systemctl enable podman.socket
  systemctl enable --now tlp
