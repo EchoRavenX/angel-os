6ac0806a617b588cd91c1495409033317c732388
EchoRavenX <MagaPenguin@proton.me>
2025-12-25 07:52:25 -0500

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,16 +9,14 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+dnf5 -y copr enable ublue-os/staging
+dnf5 swap ffmpeg-free ffmpeg --allowerasing
+
+ dnf5 update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+ dnf5 install tlp tlp-rdw
 
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+ systemctl enable --now tlp
