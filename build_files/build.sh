44c04581e8bb7e02fcaf4964d032f7ebf9dd631e
EchoRavenX <MagaPenguin@proton.me>
2025-12-26 08:19:56 -0500

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,8 +10,8 @@ dnf5 remove  -y firefox-langpacks
 dnf5 -y copr enable ublue-os/staging
 dnf5  -y swap ffmpeg-free ffmpeg --allowerasing
 dnf5 install -y \
-    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-rawhide.noarch.rpm \
-    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-rawhide.noarch.rpm
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-43.noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-43.noarch.rpm
 dnf5 group install -y multimedia --setopt=install_weak_deps=False --exclude=PackageKit-gstreamer-plugin
 
 
